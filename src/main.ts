5f152a2e236cb132414a42c9c3f2d781e484cc1b
Ingrid <ingridstanic@gmail.com>
2025-12-15 11:12:28 +0100

    added geolocation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import "./style.css";
+
+const success = (pos: GeolocationPosition) => {
+  console.log(pos.coords);
+};
+
+const error = (error: GeolocationPositionError) => {
+  console.log(error.message);
+};
+
+navigator.geolocation.getCurrentPosition(success, error);
