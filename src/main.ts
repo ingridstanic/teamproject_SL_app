40221da9908434e1fb9834ba7f82880884907700
Sami Daly <samidaly19@outlook.com>
2025-12-18 22:52:08 +0100

    in progress

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { get } from "./slLists";
 import "./style.css";
 import type { TripResponse } from "./types";
 
@@ -6,3 +5,4 @@ const data = await get<TripResponse>(
   "https://journeyplanner.integration.sl.se/v2/trips?type_origin=any&type_destination=any&name_origin=9091001000009182&name_destination=9091001000009192&calc_number_of_trips=3"
 );
 console.log(data.journeys);
+//
